705ffaa749230270f25fa46c124ab7d1c4e80f75
Lorraine <Lorraine@Isaacs-MacBook-Pro.local>
2020-09-09 17:05:32 -0500

    Updates

---
+++ precache-manifest.95aac52d25307f4cf156af766e9831bf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc9b1e8e87565912d01d54d989f1069",
+    "url": "/my-team-page/index.html"
+  },
+  {
+    "revision": "96c5a5a495162cdc012b",
+    "url": "/my-team-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98ce86382573cc456c94",
+    "url": "/my-team-page/static/js/2.692f4905.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-team-page/static/js/2.692f4905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c5a5a495162cdc012b",
+    "url": "/my-team-page/static/js/main.c33015e8.chunk.js"
+  },
+  {
+    "revision": "5d2ec80eba572448a51b",
+    "url": "/my-team-page/static/js/runtime-main.4aecf902.js"
+  },
+  {
+    "revision": "3a0be588e22cbb5e12e777896ddd98a6",
+    "url": "/my-team-page/static/media/photo1.3a0be588.png"
+  },
+  {
+    "revision": "2f3221fa6325a8a6b0ab741ea41daafb",
+    "url": "/my-team-page/static/media/photo2.2f3221fa.png"
+  },
+  {
+    "revision": "cd1a8b6dd0482cc528dd1611360069b8",
+    "url": "/my-team-page/static/media/photo3.cd1a8b6d.png"
+  },
+  {
+    "revision": "3db7eff7c671716efd014aae03661a8a",
+    "url": "/my-team-page/static/media/photo4.3db7eff7.png"
+  },
+  {
+    "revision": "f3bb02a9c381dd5e81f9224f6f77eea8",
+    "url": "/my-team-page/static/media/photo5.f3bb02a9.png"
+  },
+  {
+    "revision": "1f59f908868b16a51818a6c3c48cb015",
+    "url": "/my-team-page/static/media/photo6.1f59f908.png"
+  }
+]);
